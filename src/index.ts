b396fb23596483b31badf871ecfaacfff3030206
Avaer Kazmer <ultimus@gmail.com>
2025-02-09 00:13:05 -0800

    Update src

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { webSearch } from "./actions/webSearch";
-import type { Plugin } from "@elizaos/core";
 import { WebSearchService } from "./services/webSearchService";
 
-export const webSearchPlugin: Plugin = {
+export const webSearchPlugin = {
     name: "webSearch",
     description: "Search the web and get news",
     actions: [webSearch],
     evaluators: [],
     providers: [],
-    services: [new WebSearchService()],
+    services: [new WebSearchService() as any],
     clients: [],
+    adapters: [],
 };
 
 export default webSearchPlugin;
